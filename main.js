44c70de2672c7180539b5d54a1624a6acb2d6c41
GrigorevKirill <nollik555@gmail.com>
2024-07-21 10:08:10 +0000

    third task

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var number = 81
-console.log(number)
-console.log(number/0)
-console.log("стрка" * number)
-console.log(number * 4)
-console.log(Math.sqrt(number))
+{var number = 81;
+console.log(number);
+console.log(number/0);
+console.log("стрка" * number);
+console.log(number * 4);
+console.log(Math.sqrt(number));}
+var number1 = 3.1;
+console.log(Math.floor(number1));
+console.log(Math.ceil(number1));
+console.log(Math.round(number1));
 
-var number1 = 3.1
-console.log(Math.floor(number1))
-console.log(Math.ceil(number1))
-console.log(Math.round(number1))
+var number2 = 23
+console.log((number * 4 - 3) * 913 + 435 - 6546 )
