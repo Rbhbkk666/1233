881c2a02aa02cdb0dbbdcbbb55435e05c10aa35d
GrigorevKirill <nollik555@gmail.com>
2024-07-21 09:50:48 +0000

    second task

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("xrtvbyumo")
+var number = 81
+console.log(number)
+console.log(number/0)
+console.log("стрка" * number)
+console.log(number * 4)
+console.log(Math.sqrt(number))
+
+var number1 = 3.1
+console.log(Math.floor(number1))
+console.log(Math.ceil(number1))
+console.log(Math.round(number1))
