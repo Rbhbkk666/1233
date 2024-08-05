fbb8e1eca6b802db5c3ecbff3aa39a49b193af59
GrigorevKirill <nollik555@gmail.com>
2024-08-05 13:11:07 +0000

    nesnau

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-{var number = 81;
-console.log(number);
-console.log(number/0);
-console.log("стрка" * number);
-console.log(number * 4);
-console.log(Math.sqrt(number));}
-var number1 = 3.1;
-console.log(Math.floor(number1));
-console.log(Math.ceil(number1));
-console.log(Math.round(number1));
-
-var number2 = 23
-console.log((number * 4 - 3) * 913 + 435 - 6546 )
+var number = 34.646;
+console.log(Math.floor(number));
+console.log(Math.ceil(nuber));
+console.log(Math.round(number));
+var number1 = 6484;
+var number2 = 23;
+var number3 = 342;
+console.log(number * number1 / number2 * number3);
